d91bd00a2c73689b2faab55734b385ee627d58c4
RAFAEL LOPEZ GONZALEZ <rlg12@clL27-8>
2017-01-27 20:43:01 +0100

    proyecto dia 26/01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SwapiService} from "./swapi.service";
+
 
 @Component({
   selector: 'app-root',
+
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{
+  people = [];
+  constructor(private swapiService:SwapiService){
+
+  }
+
+  ngOnInit() {
+    this.swapiService.getPeopleList().subscribe((response) => this.people = response);
+    /*let observable:Observable<any> = this.http
+      .get(PEOPLE_API_URL);*/
+
+
+    console.log()
+
+
+  }
   title = 'app works!';
+
+
 }
